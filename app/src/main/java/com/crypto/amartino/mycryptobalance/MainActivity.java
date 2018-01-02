a1546172b0e7bd79d3c504c76beae0c08e7bd0e5
Alfonso_PCRC <martinoalfonso92@gmail.com>
2018-01-02 22:27:38 +0100

    Initial committ

--- app/src/main/java/com/crypto/amartino/mycryptobalance/MainActivity.java
+++ app/src/main/java/com/crypto/amartino/mycryptobalance/MainActivity.java
@@ -1,7 +1,9 @@
 package com.crypto.amartino.mycryptobalance;
 
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,12 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,13 +25,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     private List<Coin> coinsList = new ArrayList<>();
     private RecyclerView recyclerView;
     private CoinsAdapter mAdapter;
+    private AdView mAdView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Adversting https://developer.android.com/distribute/best-practices/earn/show-ads-admob.html
+        MobileAds.initialize(this, "ca-app-pub-3224473877526605~5449948090");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+
+
         //Improve the recycle: https://code.tutsplus.com/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
@@ -45,13 +70,8 @@ public class MainActivity extends AppCompatActivity {
         String myCoins = "[{'name':'Bitcoin','value':'11'},{'name':'OmiseGo','value':'13'}]";
         editor.putString("myCoins", myCoins);
         editor.commit();
-
-
-        try {
-            prepareList();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        prepareList();
+        mAdapter.notifyDataSetChanged();
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -64,26 +84,93 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void prepareList() throws JSONException {
-        SharedPreferences settings = getPreferences(0);
-        String coins = settings.getString("myCoins", "Not Found");
-        JSONArray jArray = new JSONArray(coins);
-        for (int i=0; i < jArray.length(); i++)
-        {
-            try {
-                JSONObject oneObject = jArray.getJSONObject(i);
-                // Pulling items from the array
-                String oneObjectsItem = oneObject.getString("name");
-                String oneObjectsItem2 = oneObject.getString("value");
-                Coin coin = new Coin(oneObjectsItem, "test", oneObjectsItem2);
-                coinsList.add(coin);
-            } catch (JSONException e) {
-                // Oops
+    private void prepareList()  {
+        //Eseguo la lettura dei valori in un thread
+
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // All your networking logic
+                // should be here
+                SharedPreferences settings = getPreferences(0);
+                String coins = settings.getString("myCoins", "Not Found");
+                JSONArray jArray = null;
+                try {
+                    jArray = new JSONArray(coins);
+                    for (int i=0; i < jArray.length(); i++)
+                    {
+                        String price = "";
+                        Log.d("LOOP","Test");
+                        JSONObject oneObject = jArray.getJSONObject(i);
+                        // Pulling items from the array
+                        String oneObjectsItem = oneObject.getString("name");
+                        String oneObjectsItem2 = oneObject.getString("value");
+
+
+                        // Create URL
+
+                        URL coinMarketCap = new URL("https://api.coinmarketcap.com/v1/ticker/"+oneObjectsItem+"/");
+                        // Create connection
+                        HttpsURLConnection myConnection =
+                                (HttpsURLConnection) coinMarketCap.openConnection();
+                        if (myConnection.getResponseCode() == 200) {
+                            InputStream responseBody = myConnection.getInputStream();
+                            InputStreamReader responseBodyReader =
+                                    new InputStreamReader(responseBody, "UTF-8");
+                            JsonReader jsonReader = new JsonReader(responseBodyReader);
+                            jsonReader.beginArray();
+                            while (jsonReader.hasNext()) {
+                                jsonReader.beginObject();
+                                while (jsonReader.hasNext()) {
+                                    String value = jsonReader.nextName();
+
+                                    //Log.d("Value",jsonReader.nextString());
+                                    if (value.equals("price_usd")) {
+
+                                        //Log.d("price",jsonReader.nextString());
+                                         price = jsonReader.nextString();
+
+
+
+                                        // price = jsonReader.nextString();
+
+                                    } else {
+                                        jsonReader.skipValue();
+                                    }
+
+                                }
+                                jsonReader.endObject();
+
+                            }
+                            jsonReader.endArray();
+
+
+
+
+
+
+
+                        } else {
+                            // Error handling code goes here
+                        }
+
+                        Coin coin = new Coin(oneObjectsItem, price, oneObjectsItem2);
+                        coinsList.add(coin);
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
-        }
+        });
 
-
-        mAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -102,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            mAdapter.notifyDataSetChanged();
             return true;
         }
 
